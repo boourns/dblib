dd228f3989158ea971d3cb2e2a7368bd28f3ffb8
Tom Burns <tom.burns@shopify.com>
2016-12-25 16:56:38 -0500

    parse database driver from scheme

--- connect.go
+++ connect.go
@@ -2,18 +2,22 @@ package dbutil
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"log"
+
+	"net/url"
+
+	_ "github.com/go-sql-driver/mysql" // mysql adapter
+	_ "github.com/mattn/go-sqlite3"    // sqlite3 adapter
 )
 
-func Connect(url string) *sql.DB {
-
-	if url == "" {
-		panic("DATABASE_URL is not set, expected  user:password@tcp(127.0.0.1:3306)/dbname")
+// Connect accepts a database url like 'mysql://user:password@tcp(127.0.0.1:3306)/dbname' or 'sqlite3://data.db'
+func Connect(dburl string) *sql.DB {
+	parsed, err := url.Parse(dburl)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	db, err := sql.Open("mysql", url)
-
+	db, err := sql.Open(parsed.Scheme, parsed.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
